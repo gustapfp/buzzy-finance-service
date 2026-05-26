543a71b4d9bb8c1660856b2845bc7da23a1366c7
gustapfp <gustavopfp.trab@gmail.com>
2026-05-26 18:36:23 -0300

    core: add infra files and setup new libs

---
+++ infra/database.ts
@@ -0,0 +1,60 @@
+import { Client, Pool } from "pg";
+import type { QueryResult, QueryResultRow } from "pg";
+import type { DatabaseStatusResponse } from "./types";
+import { DATABASE_CONFIG } from "./consts";
+
+export const createClient = (): Client => {
+  const client = new Client(DATABASE_CONFIG);
+  return client;
+};
+
+export const DB_POOL: Pool = new Pool({
+  ...DATABASE_CONFIG,
+  max: 102,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+  maxLifetimeSeconds: 60,
+});
+
+export const databaseStatus = async (): Promise<DatabaseStatusResponse> => {
+  const client = await DB_POOL.connect();
+  const dbHealthStatement = `
+  SELECT
+    (
+      SELECT count(*)::int FROM pg_stat_database
+      WHERE datname = $1
+    ) AS active_connections,
+
+    current_setting('server_version') AS server_version,
+    current_setting('max_connections')::int AS max_connections
+  `;
+
+  const updateAt = new Date().toISOString();
+
+  try {
+    const dbHealthResponse: QueryResult = await client.query({
+      text: dbHealthStatement,
+      values: [DATABASE_CONFIG.database],
+    });
+    const dbHealthRows: QueryResultRow = dbHealthResponse.rows[0];
+
+    return {
+      update_at: updateAt,
+      postgres_version: `V${dbHealthRows.server_version}`,
+      max_connections: dbHealthRows.max_connections,
+      active_connections: dbHealthRows.active_connections,
+      exit_code: 0,
+      db_message: "Database connection ok...",
+    };
+  } catch (err) {
+    console.error(err);
+    return {
+      update_at: updateAt,
+      postgres_version: "V16.0",
+      exit_code: 1,
+      db_message: String(err),
+    };
+  } finally {
+    await client.release(true);
+  }
+};
