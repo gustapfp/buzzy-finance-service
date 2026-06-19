a3c02c695dbc4ea917202bbb7ba9a6f3b644ffd5
gustapfp <gustavopfp.trab@gmail.com>
2026-06-19 15:40:29 -0300

    test: not happy path CI/CD

--- app/infra/scripts/waitForServices.ts
+++ app/infra/scripts/waitForServices.ts
@@ -5,6 +5,7 @@ export async function waitForServices() {
   await waitForExpressService();
 
   async function waitForExpressService() {
+    let gugaTest;
     process.stdout.write("💭 Connecting to Express Service...");
     return retry(fetchStatusEndpoint, {
       retries: 100,
