b5241be3bc5984f95690041fb41cccb330f9b2d4
gustapfp <gustavopfp.trab@gmail.com>
2026-06-19 15:38:55 -0300

    chore: update eslint configuration to use flat config for prettier integration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,12 +3,12 @@
 import js from "@eslint/js";
 import { defineConfig, globalIgnores } from "eslint/config";
 import tseslint from "typescript-eslint";
-
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 export default defineConfig([
   globalIgnores(["./dist"]),
   {
     files: ["**/*.{js,ts}"],
-    extends: [js.configs.recommended, tseslint.configs.recommended, "prettier"],
+    extends: [js.configs.recommended, tseslint.configs.recommended, eslintConfigPrettier],
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
     },
