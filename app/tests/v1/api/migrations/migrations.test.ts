385a9322385f394e7d7597ed1f7cb10e1637ca03
gustapfp <gustavopfp.trab@gmail.com>
2026-07-05 12:29:34 -0300

    test: update migration success status code and remove service unavailable test case

--- app/tests/v1/api/migrations/migrations.test.ts
+++ app/tests/v1/api/migrations/migrations.test.ts
@@ -43,7 +43,7 @@ describe("/migrations Endpoint", () => {
       const response = await fetch(MIGRATIONS_URL, {
         method: "POST",
       });
-      expect(response.status).toBe(201);
+      expect(response.status).toBe(200);
       const data = await response.json();
       expect(data).toHaveProperty("message");
       expect(data).toHaveProperty("applied_migrations");
